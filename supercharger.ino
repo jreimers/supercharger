06aaa54a45fa9928c80d2bd96d51420e9dda1855
John Reimers <johnanthony.reimers@gmail.com>
2014-10-18 14:58:39 -0400

    don't report state before determining state

--- supercharger.ino
+++ supercharger.ino
@@ -7,8 +7,6 @@ int charging = 0; // are we currently charging
 void loop() {
   int reading = analogRead(A0);
   double voltage = reading * 5.0 / 1024.0; // convert ADC reading to voltage
-  Serial.print(charging ? "Charging, current voltage: " : "Charge complete, voltage: ");
-  Serial.println(voltage);
   if(voltage < 2.8 && !charging) {
     digitalWrite(13, HIGH);
     charging = 1;
@@ -16,5 +14,7 @@ void loop() {
     digitalWrite(13, LOW);
     charging = 0;
   }
+  Serial.print(charging ? "Charging, current voltage: " : "Charge complete, voltage: ");
+  Serial.println(voltage);
   delay(1000);
 }
